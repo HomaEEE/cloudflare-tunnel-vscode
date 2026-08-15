fafa41f2848111a547c05a9c07bdb6fdabe125da
Arthur Boyko <homam600i@gmail.com>
2026-08-15 17:06:03 +0300

    ui: show local and public tunnel endpoints

--- src/providers/treeItems.ts
+++ src/providers/treeItems.ts
@@ -22,18 +22,24 @@ export default class CloudflareTunnelTreeItem extends vscode.TreeItem {
   // @ts-expect-error: TS2611
   override get tooltip(): vscode.MarkdownString {
     const tooltip = new vscode.MarkdownString();
+    tooltip.appendMarkdown(`**Local site**: \`${this.tunnel.localOrigin}\``);
     tooltip.appendMarkdown(
-      `\n\n**Local**: [${this.tunnel.url}](http://${this.tunnel.url})`
-    );
-    tooltip.appendMarkdown(
-      `\n\n**Tunnel**: [${this.tunnel.shortTunnelUri}](${this.tunnel.tunnelUri})`
+      `\n\n**Public**: ${
+        this.tunnel.tunnelUri
+          ? `[${this.tunnel.shortTunnelUri}](${this.tunnel.tunnelUri})`
+          : "Starting..."
+      }`
     );
     tooltip.appendMarkdown(`\n\n**Status**: ${this.tunnel.status}`);
+
     if (this.tunnel.isQuickTunnel) {
-      tooltip.appendText("\n\nQuick Tunnel");
+      tooltip.appendMarkdown("\n\n**Mode**: Quick Tunnel");
     } else {
+      tooltip.appendMarkdown(`\n\n**Public hostname**: ${this.tunnel.hostname}`);
+      tooltip.appendMarkdown(`\n\n**Herd/Valet Host**: ${this.tunnel.localHostname}`);
       tooltip.appendMarkdown(`\n\n**Tunnel name**: ${this.tunnel.tunnelName}`);
     }
+
     return tooltip;
   }
 }
