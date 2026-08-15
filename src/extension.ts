ee0c4d72124c2206043c890ba3f56bee6b2b6b80
Arthur Boyko <homam600i@gmail.com>
2026-08-15 17:05:57 +0300

    fix: clean generated configs on extension deactivation

--- src/extension.ts
+++ src/extension.ts
@@ -13,38 +13,36 @@ export async function activate(context: vscode.ExtensionContext) {
   // @ts-expect-error: TS7017
   global.testExtensionContext = context;
 
-  // State
   const globalState = GlobalState.init(context);
 
-  // Setup Cloudflared client
   cloudflared = await CloudflaredClient.init(context);
 
-  // Activate commands
   commands.forEach(callback => {
     const commandId = `${constants.prefix}.${callback.name}`;
     const command = vscode.commands.registerCommand(commandId, callback);
     context.subscriptions.push(command);
   });
 
-  // Register providers
   vscode.window.registerTreeDataProvider(
     constants.Views.list,
     cloudflareTunnelProvider
   );
 
-  // Context keys
   setContext(constants.Context.isLoggedIn, globalState.isLoggedIn);
 }
 
-// this method is called when your extension is deactivated
 export async function deactivate() {
   const { tunnels } = cloudflareTunnelProvider;
-  tunnels.forEach(async (tunnel) => {
+
+  for (const tunnel of tunnels) {
     if (tunnel.process) {
       await cloudflared.stop(tunnel);
     }
+
+    cloudflared.cleanupTunnelConfig(tunnel);
+
     if (!tunnel.isQuickTunnel) {
       await cloudflared.deleteTunnel(tunnel);
     }
-  });
+  }
 }
